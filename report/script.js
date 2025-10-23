db54429f31fd276e5e2d34920eea45e86009309a
Сабитов Омар <sabitov-omar@mail.ru>
2025-10-23 21:54:03 +0300

    Добавлен отчёт ЛР1 - Сабитов Омар

---
+++ report/script.js
@@ -0,0 +1,13 @@
+const faders=document.querySelectorAll('.fade');
+const appearOptions={threshold:0.2,rootMargin:'0px 0px -50px 0px'};
+const appearOnScroll=new IntersectionObserver(function(entries,appearOnScroll){
+entries.forEach(entry=>{if(!entry.isIntersecting)return;entry.target.classList.add('show');appearOnScroll.unobserve(entry.target);});
+},appearOptions);
+faders.forEach(fader=>{appearOnScroll.observe(fader);});
+const sections=document.querySelectorAll('section');
+const navLinks=document.querySelectorAll('nav ul.menu li a');
+window.addEventListener('scroll',()=>{
+let current='';
+sections.forEach(section=>{const sectionTop=section.offsetTop-50;if(pageYOffset>=sectionTop){current=section.getAttribute('id');}});
+navLinks.forEach(link=>{link.classList.remove('active');if(link.getAttribute('href')=='#'+current){link.classList.add('active');}});
+});
